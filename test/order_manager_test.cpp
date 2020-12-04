f97e01a44eea02e2bb13e4637a35d33b069c1748
Govind <govindajithkumar@gmail.com>
2020-12-04 17:50:51 -0500

    [Task 2.4] initial tests for order_manager done

--- test/order_manager_test.cpp
+++ test/order_manager_test.cpp
@@ -2,14 +2,28 @@
 #include "ros/ros.h"
 #include "../include/order_manager.h"
 
-TEST(order_manager_test,checkOrderSize) {
+TEST(order_size_test,checkOrderSize) {
     OrderManager manager;
     manager.generateOrder();
     EXPECT_EQ(manager.getOrderSize(),4);
 }
 
-TEST(order_manager_test,checkTotalCubes) {
+TEST(cube_test,checkTotalCubes) {
     OrderManager manager;
     manager.spawnCubes();
     EXPECT_EQ(manager.getTotalCubes(),8);
+}
+
+TEST(cube_vec_test,checkTotalCubes) {
+    OrderManager manager;
+    std::vector<char> order;
+    order = manager.getOrder();
+    EXPECT_EQ(order.size(),4);
+}
+
+TEST(order_vec_test,checkTotalCubes) {
+    OrderManager manager;
+    std::vector<char> cubes;
+    cubes = manager.getCubes();
+    EXPECT_EQ(cubes.size(),8);
 }
