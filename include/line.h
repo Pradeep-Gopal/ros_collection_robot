00e3fdc04cdf92711cc2bf63da7fa190609fe964
Govind <govindajithkumar@gmail.com>
2020-12-05 08:26:45 -0500

    [Task 2.7] edited private variable notations

--- include/line.h
+++ include/line.h
@@ -7,9 +7,9 @@ class Line{
 
 private:
     geometry_msgs::Point end_;
-    double a;
-    double b;
-    double c;
+    double a_;
+    double b_;
+    double c_;
 public:
     //edited this in the uml diagram as well, constructor cant have a void return type
     Line(geometry_msgs::Point a, geometry_msgs::Point b, geometry_msgs::Point c);
