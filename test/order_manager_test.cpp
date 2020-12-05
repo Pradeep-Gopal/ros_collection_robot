ed0bbfa89a7d63f1f7e41d720f76a250cf8f66ba
Govind <govindajithkumar@gmail.com>
2020-12-05 15:03:18 -0500

    [Task 2.4] Unit test for order manager succeeded

--- test/order_manager_test.cpp
+++ test/order_manager_test.cpp
@@ -1,28 +1,9 @@
 #include <gtest/gtest.h>
 #include "../include/order_manager.h"
 
-TEST(order_size_test,checkOrderSize) {
+TEST(generateOrder,orderSizeCheck) {
     OrderManager manager;
     manager.generateOrder();
-    EXPECT_EQ(manager.getOrderSize(),4);
-}
-
-TEST(cube_test,checkTotalCubes) {
-    OrderManager manager;
-    manager.spawnCubes();
-    EXPECT_EQ(manager.getTotalCubes(),8);
-}
-
-TEST(cube_vec_test,checkTotalCubes) {
-    OrderManager manager;
-    std::vector<char> order;
-    order = manager.getOrder();
-    EXPECT_EQ(order.size(),4);
-}
-
-TEST(order_vec_test,checkTotalCubes) {
-    OrderManager manager;
-    std::vector<char> cubes;
-    cubes = manager.getCubes();
-    EXPECT_EQ(cubes.size(),8);
+    EXPECT_EQ(manager.getOrder().size(),manager.getOrderSize());
+    EXPECT_EQ(manager.getCubes().size(),manager.getTotalCubes());
 }
