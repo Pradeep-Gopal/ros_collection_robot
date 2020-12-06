b4ab84fd6a8bfb1130323bcac8077c79d4d042d8
Govind <govindajithkumar@gmail.com>
2020-12-06 14:44:41 -0500

    [Task 2.18] Writing functions for reading and parsing YAML files and creating polygons from rectangle and circle

--- include/map.h
+++ include/map.h
@@ -3,16 +3,20 @@
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Point.h"
 #include "ros/ros.h"
+#include <yaml-cpp/yaml.h>
 #include "../include/polygon.h"
 
 class Map{
-private:
-    std::vector<Polygon> obstacles_;
-    std::vector<Polygon> offset_obstacles_;
-    double clearance_;
-public:
-    bool insideObstacle(geometry_msgs::Point);
-    Polygon polyFromRect(std::map<std::string,double>);
-    Polygon polyFromCircle(std::map<std::string,double>);
-    Polygon offsetPolygon(Polygon);
+    private:
+        std::vector<Polygon> obstacles_;
+        std::vector<Polygon> offset_obstacles_;
+        double clearance_;
+    public:
+        Map(std::string, double);
+        void parseYAML(std::string);
+        bool insideObstacle(geometry_msgs::Point);
+        Polygon polyFromRect(double, double, double, double, double);
+        Polygon polyFromCircle(double, double, double);
+        Polygon offsetPolygon(Polygon);
 };
+
--- src/map.cpp
+++ src/map.cpp
@@ -3,19 +3,83 @@
 #include "ros/ros.h"
 #include "../include/map.h"
 
-bool Map::insideObstacle(geometry_msgs::Point check_point){
+void Map::parseYAML(std::string fname) {
+    YAML::Node map = YAML::LoadFile(fname);
+    for (auto it = map.begin(); it != map.end(); ++it){
+        YAML::Node key = it->first;
+        YAML::Node obs = map[key.as<std::string>()];
+        if (obs["type"].as<std::string>() == "rectangle"){
+            std::vector<double> start = obs["start"].as<std::vector<double>>();
+            double width = obs["width"].as<double>();
+            double height = obs["height"].as<double>();
+            double theta = obs["theta"].as<double>();
 
+            obstacles_.push_back(polyFromRect(start[0],start[1],width,height,theta));
+        }
+        else if (obs["type"].as<std::string>() == "circle"){
+            std::vector<double> center = obs["center"].as<std::vector<double>>();
+            double radius = obs["radius"].as<double>();
+            obstacles_.push_back(polyFromCircle(center[0],center[1],radius));
+        }
+        else if (obs["type"].as<std::string>() == "polygon") {
+            YAML::Node coords = obs["vertices"];
+            std::vector <geometry_msgs::Point> vertices;
+            for(int i = 0; i < coords.size(); ++i){
+                geometry_msgs::Point vertex;
+                vertex.x = coords[i][0].as<double>();
+                vertex.y = coords[i][1].as<double>();
+                vertices.push_back(vertex);
+            }
+            obstacles_.push_back(Polygon(vertices));
+        }
+    }
 }
 
-Polygon Map::polyFromRect(std::map<std::string,double> rectangle ){
 
+Polygon Map::polyFromRect(double xs, double ys, double width, double height, double theta){
+    std::vector <geometry_msgs::Point> vertices;
+
+    const double pi = 3.14159;
+    double theta_rad = theta * (pi/180);
+    double l = sqrt(pow(height,2)+pow(width,2));
+    double alpha = atan2(height,width);
+
+    geometry_msgs::Point point;
+    point.x = xs;
+    point.y = ys;
+    vertices.push_back(point);
+
+    point.x = xs+(width*cos(theta_rad));
+    point.y = ys+(width*sin(theta_rad));
+    vertices.push_back(point);
+
+    point.x = xs+(l*cos(theta_rad+alpha));
+    point.y = ys+(l*sin(theta_rad+alpha));
+    vertices.push_back(point);
+
+    point.x = xs+(height*cos(theta_rad+(pi/2)));
+    point.y = ys+(height*sin(theta_rad+(pi/2)));
+    vertices.push_back(point);
+
+    return Polygon(vertices);
 }
 
-Polygon Map::polyFromCircle(std::map<std::string,double> circle){
+Polygon Map::polyFromCircle(double xc, double yc, double radius){
+    std::vector <geometry_msgs::Point> vertices;
+    geometry_msgs::Point point;
 
+    const double pi = 3.14159;
+
+    int sides = 24;
+    double theta = 0;
+    for (int i = 0; i<sides; i++) {
+        point.x = xc + (radius * sin(theta));
+        point.y = yc + (radius * cos(theta));
+        theta += (2*pi)/sides;
+        vertices.push_back(point);
+    }
+
+    return Polygon(vertices);
 }
 
-Polygon Map::offsetPolygon(Polygon poly){
-
-}
 
