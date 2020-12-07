660c5449f6375f96a48d79a83be42ed8304df713
Govind <govindajithkumar@gmail.com>
2020-12-07 07:11:42 -0500

    [Task 2.21] Spawning cubes at random points in the world, not close to each other

--- src/order_manager.cpp
+++ src/order_manager.cpp
@@ -1,9 +1,8 @@
 #include "../include/order_manager.h"
 
-
 void OrderManager::generateOrder(){
-	ROS_INFO_STREAM("Generating Order ...");
-	std::vector<char>available_cubes = {'A','B','C','D','E','F','G','H'};
+    ROS_INFO_STREAM("Generating Order ...");
+    std::vector<char>available_cubes = {'A','B','C','D','E','F','G','H'};
     int random_cube_idx;
     srand(time(NULL));// to generate a truly random number every time
     for(int i = 0; i < total_cubes_; i++){
@@ -30,61 +29,74 @@ void OrderManager::generateOrder(){
     ROS_INFO_STREAM("Random Cubes generated ...");
 }
 
-void OrderManager::spawnCubes(){
-    std::vector<geometry_msgs::Point> random_locations;
-    geometry_msgs::Point random_point;
-
+void OrderManager::spawnCubes() {
     srand(time(NULL));
-    random_point.x = (double)(rand() % (1500))/100;
-    random_point.y = (double)(rand() % (1500))/100;
-    ROS_INFO_STREAM("random_point.x : "<<random_point.x);
-    ROS_INFO_STREAM("random_point.y : "<<random_point.y);
-    ROS_INFO_STREAM(map_object_.insideObstacle(random_point));
-//    random_locations.push_back(random_point);
-//    ROS_INFO_STREAM(random_point);
-//    ros::ServiceClient spawn_object = nh_.serviceClient<gazebo_msgs::SpawnModel>("gazebo/spawn_sdf_model");
-//    gazebo_msgs::SpawnModel spawn;
-//    spawn.request.model_name="box";
-//
-//    std::string path = ros::package::getPath("ros_collection_robot");
-//    std::string model_path = path + "/models/cube_A/model.sdf";
-//    std::ifstream ifs(model_path);
-//
-//    std::string xml = "";
-//    for(std::string line; std::getline(ifs,line); )
-//    {
-//        xml += line;
-//    }
-//
-//    spawn.request.model_xml = xml;
-//    geometry_msgs::Pose pose;
-//
-//    pose.position.x=random_point.x;
-//    pose.position.y=random_point.y;
-//    pose.position.z=random_point.z;
-//    pose.orientation.x=0;
-//    pose.orientation.y=0;
-//    pose.orientation.z=0;
-//    pose.orientation.w=1;
-//
-//    spawn.request.initial_pose = pose;
-//
-//    if (!spawn_object.call(spawn)) {
-//        ROS_INFO_STREAM("Failed to call service %s");
-//    }
-//    ROS_INFO_STREAM(spawn.response.status_message);
+
+    std::string model_dir = ros::package::getPath("ros_collection_robot") + "/models";
+
+    ros::ServiceClient spawn_client = nh_.serviceClient<gazebo_msgs::SpawnModel>("gazebo/spawn_sdf_model");
+    gazebo_msgs::SpawnModel spawn;
+
+    double min_dist = 2;
+    std::vector<geometry_msgs::Point> locations;
+    int count = 0;
+    for (char c:cubes_) {
+        geometry_msgs::Point pt;
+        while (true) {
+            pt.x = (double) (rand() % (1500)) / 100;
+            pt.y = (double) (rand() % (1500)) / 100;
+            if (!map_object_.insideObstacle(pt)) {
+                bool too_close = false;
+                for (geometry_msgs::Point loc:locations){
+                    double dist = sqrt(pow(pt.x-loc.x,2)+pow(pt.y-loc.y,2));
+                    if (dist <= min_dist) {
+                        too_close = true;
+                    }
+                }
+                if(!too_close) {
+                    locations.push_back(pt);
+                    break;
+                }
+            }
+        }
+
+        std::string name = "cube_";
+        name.push_back(c);
+        name += std::to_string(count);
+        spawn.request.model_name = name;
+
+        std::string model_path = model_dir + "/cube_" + c + "/model.sdf";
+        std::ifstream ifs(model_path);
+        std::string xml = "";
+        for (std::string line; std::getline(ifs, line);)
+            xml += line;
+        spawn.request.model_xml = xml;
+
+        geometry_msgs::Pose pose;
+        pose.position.x = pt.x;
+        pose.position.y = pt.y;
+        pose.position.z = pt.z;
+
+        spawn.request.initial_pose = pose;
+        ROS_INFO_STREAM(name << " (" << pt.x << ", " << pt.y << ")");
+
+        spawn_client.call(spawn);
+        count++;
+    }
 }
 
 int OrderManager::getTotalCubes(){
-	return total_cubes_;
+    return total_cubes_;
 }
 
 int OrderManager::getOrderSize(){
-	return order_size_;
+    return order_size_;
 }
+
 std::vector<char> OrderManager::getOrder(){
-	return order_;
+    return order_;
 }
+
 std::vector<char> OrderManager::getCubes(){
-	return cubes_;
+    return cubes_;
 }
